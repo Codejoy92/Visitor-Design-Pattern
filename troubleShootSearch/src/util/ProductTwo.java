f389f54da2ffdfcf7caf481f2b3d3fa16b8413c3
ChaitanyaKhoje <chaitanyakhoje9@gmail.com>
2018-04-20 20:28:42 -0400

    added product classes

---
+++ troubleShootSearch/src/util/ProductTwo.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductTwo extends dSeaGate implements VisitorAcceptanceI {
+
+    private static List<String> productTwoList = new ArrayList<String>();
+
+    public ProductTwo() {}
+
+    @Override
+    public void accept(VisitorI visitorI) {
+        visitorI.visit(this);
+    }
+
+    public List<String> getProductTwoList() {
+        return productTwoList;
+    }
+
+    public void setProductTwoList(List<String> productTwoList) {
+        this.productTwoList = productTwoList;
+    }
+}
